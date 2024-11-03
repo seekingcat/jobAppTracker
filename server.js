46a3dbde7bfd0ca23bead134fd2c21e609fe0082
seekingcat <llana_a154@yahoo.co.uk>
2024-11-03 21:55:37 +0000

    local mongoose connection and middleware setup

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ const mongoose = require('mongoose');
 const Meet = require('./models/model')
 const methodOverride = require('method-override')
 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(dbConnectionString);
+  console.log('CONNECTED TO DATABASE')
+}
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(methodOverride('_method'))
+
 app.listen(PORT || process.env.port, () => {
     console.log(`NOW LISTENING ON PORT ${PORT}`)
 })
